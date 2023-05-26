2fcd843e609f58f969aa0a1db8cdebfc5f6ba047
PrattyBee <pratyush.kaintura@hotmail.com>
2023-05-26 17:25:33 -0400

    Fix bug causing homepage to crash

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -8,10 +8,6 @@ export const verifyToken = async (req, res, next) => {
       return res.status(403).send("Access Denied");
     }
 
-    if (token.startsWith("Bearer ")) {
-      token = token.slice(7, tokens.length).trimLeft();
-    }
-
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     req.user = verified;
     next();
